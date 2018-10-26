b9f7a67f0bd32a40dc4ab20751d3278be0146aa4
bikash488 <44465080+bikash488@users.noreply.github.com>
2018-10-26 17:57:57 +0530

    Update ftat.java

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -1,6 +1,7 @@
 package functionaltest;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.By;
 //import org.openqa.selenium.WebDriver;
